debe545551ebf5a179243582f24ed00f539883ff
Sanskar <47107112+sanskarsharma33@users.noreply.github.com>
2020-05-28 02:09:28 +0530

    Update urls.py

--- backend/src/challenge_app/api/urls.py
+++ backend/src/challenge_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('challenge/', views.ChallengeDetails.as_view(), name='challenge'),
-    path('challenge-list/', views.ChallengeList.as_view(), name='challenge-list'),
-    path('challenge-edit/<int:challenge_id>)/', views.ChallengeEdit.as_view(), name='challenge-edit'),
+    path('challenge/', login_required(views.ChallengeDetails.as_view()), name='challenge'),
+    path('challenge-list/', login_required(views.ChallengeList.as_view()), name='challenge-list'),
+    path('challenge-edit/<int:challenge_id>)/', login_required(views.ChallengeEdit.as_view()), name='challenge-edit'),
 ]
